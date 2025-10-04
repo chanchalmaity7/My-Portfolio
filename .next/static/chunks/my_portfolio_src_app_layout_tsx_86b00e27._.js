87b24c4c18992ef83020ed91965a60aa72260f97
chanchal maity <maitychanchal7@gmail.com>
2025-10-05 00:47:58 +0530

    gitignore

--- .next/static/chunks/my_portfolio_src_app_layout_tsx_86b00e27._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__ceca4a0e._.css"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/my_portfolio_src_app_page_tsx_155b913e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/my_portfolio_src_components_78743142._.js",
-  "static/chunks/3a8df_21615507._.js"
-],
-    source: "dynamic"
-});
